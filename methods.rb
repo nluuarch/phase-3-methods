799e2d68a9e435bbcac6f6279cef4fc0b0ebce0e
nluuarch@gmail.com <nluuarch@gmail.com>
2021-12-12 16:44:45 -0800

    commit tree 4ac90147d73787e17d8e20c8ea35b30578757607

--- methods.rb
+++ methods.rb
@@ -1,1 +1,60 @@
-# Your code here!
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default()
+
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(2, 5)
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+def halve(number)
+    if number.class == String 
+        return nil
+    end
+
+    number / 2
+end
+
+halve(6)
+halve("six")
